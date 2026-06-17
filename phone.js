61e1bafeb6da373afe389faafa42749f9782c2a9
sidwokenup_ <wakedupsid2023@gmail.com>
2026-06-17 06:17:12 +0530

    phonejs added

---
+++ phone.js
@@ -0,0 +1,6 @@
+// ---------------------------------------------------------
+// Change the phone number inside the quotes below.
+// It will automatically update everywhere on the website.
+// ---------------------------------------------------------
+
+window.SITE_PHONE_NUMBER = "+1 (833) 626-2983";
